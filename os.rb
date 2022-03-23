f94c6ac472d2a0a0774cc4152ff6cf81c6f53c24
KagemniKarimu <kagemnikarimu>
2022-03-23 11:38:09 -0500

    updated OS module to include more windows types, and refactored get_os method

--- os.rb
+++ os.rb
@@ -2,7 +2,7 @@
 
 module OS
   def OS.windows?
-    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+    (/cygwin|mswin|mingw|bccwin|wince|emx|djgpp/ =~ RUBY_PLATFORM) != nil
   end
 
   def OS.mac?
@@ -17,14 +17,13 @@ module OS
     OS.unix? and !OS.mac?
   end
 
-  def OS.jruby?
-    RUBY_ENGINE == 'jruby'
-  end
-
   def OS.get_os
-    return "Windows #{RUBY_PLATFORM}" if OS.windows?
-    return "Linux #{RUBY_PLATFORM}" if OS.linux?
-    return "Unix #{RUBY_PLATFORM}" if OS.unix?
-    return "Mac #{RUBY_PLATFORM}" if OS.mac?
+    case
+    when OS.windows? then "Windows #{RUBY_PLATFORM}"
+    when OS.linux? then "Linux #{RUBY_PLATFORM}"
+    when OS.unix? then "Unix #{RUBY_PLATFORM}"
+    when OS.mac? then "Mac #{RUBY_PLATFORM}"
+    else "Unknown #{RUBY_PLATFORM}"
+    end
   end
 end
